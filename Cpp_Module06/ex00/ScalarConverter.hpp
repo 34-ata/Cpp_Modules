5073acdab753d3d06f214e71704c481ff05e3a52
Fatih Mehmet Ata <faata@k1m38s01.42istanbul.com.tr>
2025-02-15 18:49:02 +0300

    Ex00 of module06 added.

--- Cpp_Module06/ex00/ScalarConverter.hpp
+++ Cpp_Module06/ex00/ScalarConverter.hpp
@@ -1,17 +1,18 @@
-#ifndef SCALAR_CONVERTER_HPP
-#define SCALAR_CONVERTER_HPP
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
 
+#include <iostream>
 #include <string>
+#include <cmath>
+#include <limits>
 
 class ScalarConverter
 {
 	public:
 		ScalarConverter();
+		ScalarConverter(const ScalarConverter &src);
 		~ScalarConverter();
-		ScalarConverter(const ScalarConverter &other);
-		ScalarConverter &operator=(const ScalarConverter &other);
-
-	public:
+		ScalarConverter &operator=(const ScalarConverter &src);
 		static void convert(const std::string &str);
 };
 
